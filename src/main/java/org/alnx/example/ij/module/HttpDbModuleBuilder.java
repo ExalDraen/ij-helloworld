227fe6e604a51f5a76bd5582a3f7118ab76bce93
Alexander Hermes <alexander.hermes@alnx.org>
2022-03-07 22:54:18 +0800

    A little bit of wiring up for module building

--- src/main/java/org/alnx/example/ij/module/HttpDbModuleBuilder.java
+++ src/main/java/org/alnx/example/ij/module/HttpDbModuleBuilder.java
@@ -4,7 +4,6 @@ import com.intellij.ide.util.projectWizard.ModuleBuilder;
 import com.intellij.ide.util.projectWizard.ModuleWizardStep;
 import com.intellij.ide.util.projectWizard.WizardContext;
 import com.intellij.openapi.Disposable;
-import com.intellij.openapi.module.ModuleType;
 import com.intellij.openapi.options.ConfigurationException;
 import com.intellij.openapi.roots.ModifiableRootModel;
 import org.jetbrains.annotations.NotNull;
@@ -12,9 +11,12 @@ import org.jetbrains.annotations.Nullable;
 
 public class HttpDbModuleBuilder extends ModuleBuilder {
 
+    String db;
+
     @Override
     public void setupRootModel(@NotNull ModifiableRootModel modifiableRootModel) throws ConfigurationException {
-
+        // this is where we set up the source , test folders, sdk, etc.
+        // instantiate vfs here
     }
 
     @Override
@@ -22,10 +24,8 @@ public class HttpDbModuleBuilder extends ModuleBuilder {
         return HttpDbModuleType.getInstance();
     }
 
-
-
     @Override
     public @Nullable ModuleWizardStep getCustomOptionsStep(WizardContext context, Disposable parentDisposable) {
-        return new HttpDbWizardStep();
+        return new HttpDbWizardStep(this);
     }
 }
--- src/main/java/org/alnx/example/ij/module/HttpDbWizardStep.java
+++ src/main/java/org/alnx/example/ij/module/HttpDbWizardStep.java
@@ -1,10 +1,19 @@
 package org.alnx.example.ij.module;
 
+import com.intellij.ide.util.PropertiesComponent;
 import com.intellij.ide.util.projectWizard.ModuleWizardStep;
 
 import javax.swing.*;
 
+import static org.alnx.example.ij.services.SidecarService.DB_SETTING_NAME;
+
 public class HttpDbWizardStep extends ModuleWizardStep {
+    private final HttpDbModuleBuilder builder;
+
+    public HttpDbWizardStep(HttpDbModuleBuilder builder) {
+        this.builder = builder;
+    }
+
     @Override
     public JComponent getComponent() {
         return new JLabel("Something something something.");
@@ -13,5 +22,6 @@ public class HttpDbWizardStep extends ModuleWizardStep {
     @Override
     public void updateDataModel() {
         // update the data model in the UI, e.g. names and directories and settings type
+        builder.db = "foo bar baz";  // TODO: actually prompt for db name and provide it here
     }
 }
